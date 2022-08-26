c4077bb8067973fff47706998f5ce7dff468dde8
Boris Xenofontov <bxenofontov@sharplogic.ai>
2022-08-26 10:28:55 +0300

    Edit admin components

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -3,25 +3,34 @@ import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
 import { LoginPageComponent } from "./login-page/login-page.component";
 import { AdminLayoutComponent } from "./shared/components/admin-layout/admin-layout.component";
+import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
+import { CreatePageComponent } from './create-page/create-page.component';
+import { EditPageComponent } from './edit-page/edit-page.component';
 
 @NgModule({
+    declarations: [
+        AdminLayoutComponent,
+        LoginPageComponent,
+        DashboardPageComponent,
+        CreatePageComponent,
+        EditPageComponent
+    ],
     imports: [
         CommonModule,
         RouterModule.forChild([
             {
                 path: '', component: AdminLayoutComponent, children: [
                     {path: '', redirectTo: '/admin/login', pathMatch: 'full'},
-                    {path: 'login', component: LoginPageComponent}
+                    {path: 'login', component: LoginPageComponent},
+                    {path: 'dashboard', component: DashboardPageComponent},
+                    {path: 'create', component: CreatePageComponent},
+                    {path: 'post/:id/edit', component: EditPageComponent}
                 ]
             }
         ])
     ],
     exports: [
         RouterModule
-    ],
-    declarations: [
-        AdminLayoutComponent,
-        LoginPageComponent
     ]
 })
 export class AdminModule {
