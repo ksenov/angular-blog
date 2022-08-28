bc74712576e8ef5301aaeaa9ed7dc4945e87074d
Boris Xenofontov <bxenofontov@sharplogic.ai>
2022-08-28 18:57:15 +0300

    Token storage

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -11,6 +11,7 @@ import { AuthService } from '../shared/services/auth.service';
 })
 export class LoginPageComponent implements OnInit {
   form: FormGroup;
+  submitted = false;
 
   constructor(
     private auth: AuthService,
@@ -35,6 +36,8 @@ export class LoginPageComponent implements OnInit {
       return
     }
 
+    this.submitted = true
+
     const user: User = {
       email: this.form.value.email,
       password: this.form.value.password
@@ -43,6 +46,7 @@ export class LoginPageComponent implements OnInit {
     this.auth.login(user).subscribe(() => {
       this.form.reset()
       this.router.navigate(['/admin', 'dashboard'])
+      this.submitted = false
     })
   }
 
