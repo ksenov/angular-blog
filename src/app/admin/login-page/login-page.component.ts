1883c4b6c7b0a9f3ebe6421d96381264cc9338ce
Boris Xenofontov <xenofontb@gmail.com>
2022-08-30 22:08:45 +0300

    Error handling

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -14,7 +14,7 @@ export class LoginPageComponent implements OnInit {
   submitted = false;
 
   constructor(
-    private auth: AuthService,
+    public auth: AuthService,
     private router: Router
   ) { }
 
@@ -47,8 +47,8 @@ export class LoginPageComponent implements OnInit {
       this.form.reset()
       this.router.navigate(['/admin', 'dashboard'])
       this.submitted = false
+    }, () => {
+      this.submitted = false
     })
   }
-  // test commit
-  // test commit 2
 }
