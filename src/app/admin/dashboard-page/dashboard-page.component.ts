c980570a8442798803bbfeaa0f051d768acb66bc
Boris Xenofontov <xenofontb@gmail.com>
2022-09-03 13:40:39 +0300

    Add post remove func

--- src/app/admin/dashboard-page/dashboard-page.component.ts
+++ src/app/admin/dashboard-page/dashboard-page.component.ts
@@ -13,6 +13,7 @@ export class DashboardPageComponent implements OnInit {
 
   posts: Post[] = []
   pSub: Subscription
+  dSub: Subscription
   searchStr = ''
 
   constructor(
@@ -20,18 +21,23 @@ export class DashboardPageComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    this.postsService.getAll().subscribe(posts => {
+    this.pSub = this.postsService.getAll().subscribe(posts => {
       this.posts = posts
     })
   }
 
   remove(id: string) {
-
+    this.dSub = this.postsService.remove(id).subscribe(() => {
+      this.posts = this.posts.filter(post => post.id !== id)
+    })
   }
 
   ngOnDestroy() {
     if (this.pSub) {
       this.pSub.unsubscribe()
+    }
+    if (this.dSub) {
+      this.dSub.unsubscribe()
     }
   }
 
